40edf7fd91a49c29eae4e175cf105511ec124d86
walu-dev <jonathanenoch11@gmail.com>
2025-06-03 14:33:11 +0300

    Declare double variable named distance
    Declare a double variable named distance and assign it the value 12345.6789.

--- Task_1/main.c
+++ Task_1/main.c
@@ -9,5 +9,8 @@ int main()
     float height = 5.9;
     printf("The height is: %f\n",height);
 
+    double distance = 12345.6789;
+    printf("The distance is: %lf\n",distance);
+
     return 0;
 }
